4412a4388101693cfbbbe4dbbe1a243b6b74c40b
giangbb <giangtpu@gmail.com>
2023-02-13 08:17:24 +0700

    update src

--- 01-node-farm/app.js
+++ 01-node-farm/app.js
@@ -3,14 +3,41 @@ const fs = require('fs');
 
 
 //Method 2: read the data once when starting app
-const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const jsonData = JSON.parse(data);
+
+const templateOverviews = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const templateCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
+
+const replaceTemplates = (template, product) => {
+    let output =  template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
     const pathName = req.url;
     if (pathName === '/overview'){
-        res.end('This is OVERVIEW');
+
+        //method 1
+        const cards = jsonData.map(d => replaceTemplates(templateCard, d));
+
+        //method 2
+        // let cards = [];
+        // for (let i = 0; i < jsonData.length ; i++){
+        //     const product = jsonData[i];
+        //     cards.push(replaceTemplates(templateCard, product));
+        // }
+        let output = templateOverviews.replace(/{%PRODUCT_CARD%/g, cards.join(''));
+
+        res.writeHead(200, {
+            'Content-type': 'text/html', // Content is HTML
+        })
+        res.end(output)
     }else if (pathName === '/product'){
         res.end('This is PRODUCT');
     }else if (pathName === '/api'){
