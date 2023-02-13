e6bbe4403448e584e757a153a25e3ff7a5185dc6
giangbb <giangtpu@gmail.com>
2023-02-13 09:13:09 +0700

    update src

--- 01-node-farm/app.js
+++ 01-node-farm/app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 
 //Method 2: read the data once when starting app
@@ -8,20 +9,32 @@ const jsonData = JSON.parse(data);
 
 const templateOverviews = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
 const templateCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
+const templateProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
 
 const replaceTemplates = (template, product) => {
     let output =  template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%FROM%}/g, product.from);
     output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
     output = output.replace(/{%PRICE%}/g, product.price);
     output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
     output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
     return output;
 }
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    const pathName = req.url;
-    if (pathName === '/overview'){
+    // const pathName = req.url;
+    // console.log(url.parse(req.url, true))
+
+    // const obj =  url.parse(req.url, true);
+    // let pathname = obj.pathname;
+    // let query = obj.query;
+    const {query, pathname} = url.parse(req.url, true);
+
+    if (pathname === '/overview'){
 
         //method 1
         const cards = jsonData.map(d => replaceTemplates(templateCard, d));
@@ -38,9 +51,17 @@ const server = http.createServer((req, res) => {
             'Content-type': 'text/html', // Content is HTML
         })
         res.end(output)
-    }else if (pathName === '/product'){
-        res.end('This is PRODUCT');
-    }else if (pathName === '/api'){
+    }
+    else if (pathname === '/product'){
+        const product = jsonData[query.id*1];
+        const output = replaceTemplates(templateProduct,product);
+
+        res.writeHead(200, {
+            'Content-type': 'text/html', // Content is HTML
+        })
+        res.end(output);
+    }
+    else if (pathname === '/api'){
 
         //Method 1: read the data every time request come in
         // fs.readFile('./dev-data/data.json', 'utf-8', (err, data) => {
