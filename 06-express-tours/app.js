dac1f89f747b0b85f653f95ff8267939bbae4f13
giangbb <giangtpu@gmail.com>
2023-02-20 08:10:38 +0700

    implement basic routing

--- 06-express-tours/app.js
+++ 06-express-tours/app.js
@@ -2,19 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+const indexHandler = (req, res) => {
     // res.status(200).send('Hello from sv');
-
     const result = {
         message: 'Hello from sv',
         app: 'tours',
     };
-
     res.status(200).json(result);
-});
+};
+
+app.get('/', indexHandler);
 
 app.listen(8080, () => {
     console.log('App running on port 8080');
 });
-
-// https://www.postman.com/
