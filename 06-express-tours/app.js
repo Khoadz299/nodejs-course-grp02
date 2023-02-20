a3ae15253363820d02b4e743aeea2ed8bd687dc0
giangbb <giangtpu@gmail.com>
2023-02-20 09:28:02 +0700

    implement basic RESET API

--- 06-express-tours/app.js
+++ 06-express-tours/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const fs = require('fs');
+
+const toursData = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8');
+const tours = JSON.parse(toursData);
 
 const app = express();
+app.use(express.json());
 
 const indexHandler = (req, res) => {
     // res.status(200).send('Hello from sv');
@@ -11,7 +16,39 @@ const indexHandler = (req, res) => {
     res.status(200).json(result);
 };
 
-app.post('/', indexHandler);
+app.get('/', indexHandler);
+
+//CRUD OPERATIONS
+const getAllTours = (req, res) => {
+    res.status(200).json({
+        // tours: tours
+        tours
+    })
+}
+
+const getTour = (req, res) => {
+    console.log(req.params);
+    const tour = tours[req.params.id*1];
+    res.status(200).json({
+        tour
+    })
+}
+
+const createTour = (req, res) => {
+    console.log(req.body);
+
+    res.status(200).json({
+        msg: "ok",
+    })
+}
+
+app.get('/api/v1/tours', getAllTours);
+app.get('/api/v1/tours/:id', getTour);
+app.post('/api/v1/tours', createTour);
+
+
+
+
 
 app.listen(8080, () => {
     console.log('App running on port 8080');
