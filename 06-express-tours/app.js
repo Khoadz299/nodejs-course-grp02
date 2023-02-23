bbc72d3d8190716d9e5e2bca1a295affe02cc9bf
giangbb <giangtpu@gmail.com>
2023-02-23 09:18:33 +0700

    - implement CRUD for tours resource - move handlers to controllers

--- 06-express-tours/app.js
+++ 06-express-tours/app.js
@@ -1,54 +1,26 @@
 const express = require('express');
-const fs = require('fs');
-
-const toursData = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8');
-const tours = JSON.parse(toursData);
-
 const app = express();
 app.use(express.json());
 
-const indexHandler = (req, res) => {
-    // res.status(200).send('Hello from sv');
-    const result = {
-        message: 'Hello from sv',
-        app: 'tours',
-    };
-    res.status(200).json(result);
-};
+const tourController = require('./controllers/tour');
 
-app.get('/', indexHandler);
+//CRUD OPERATIONS - create read update delete
+//method 2: combine same routes but different handlers
+app.route('/api/v1/tours')
+    .get(tourController.getAllTours)
+    .post(tourController.createTour)
 
-//CRUD OPERATIONS
-const getAllTours = (req, res) => {
-    res.status(200).json({
-        // tours: tours
-        tours
-    })
-}
+app.route('/api/v1/tours/:id')
+    .get(tourController.getTour)
+    .delete(tourController.deleteTour)
+    .patch(tourController.updateTour)
 
-const getTour = (req, res) => {
-    console.log(req.params);
-    const tour = tours[req.params.id*1];
-    res.status(200).json({
-        tour
-    })
-}
-
-const createTour = (req, res) => {
-    console.log(req.body);
-
-    res.status(200).json({
-        msg: "ok",
-    })
-}
-
-app.get('/api/v1/tours', getAllTours);
-app.get('/api/v1/tours/:id', getTour);
-app.post('/api/v1/tours', createTour);
-
-
-
-
+//method 1: split routes
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+// app.patch('/api/v1/tours/:id', updateTour);
 
 app.listen(8080, () => {
     console.log('App running on port 8080');
---
+++ 06-express-tours/controllers/tour.js
@@ -0,0 +1,132 @@
+const fs = require('fs');
+const toursData = fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8');
+const tours = JSON.parse(toursData);
+
+//CRUD OPERATIONS
+exports.getAllTours = (req, res) => {
+    res.status(200).json({
+        code: 200,
+        msg: `OK`,
+        data: {
+            tours
+        }
+    })
+}
+
+exports.getTour = (req, res) => {
+    console.log(req.params);
+    const id = req.params.id*1;
+    const index = tours.findIndex((tour) => {return tour.id === id});
+    const tour = tours[index];
+    if (index >= 0){
+        res.status(200).json({
+            code: 200,
+            msg: `OK`,
+            data: {
+                tour
+            }
+        })
+    }else{
+        res
+            .status(404) //HTTP CODE 404 -NOT FOUND
+            .json({
+                code: 404,
+                msg: `Not found tour with ${id}!`
+            })
+    }
+
+}
+
+exports.createTour = (req, res) => {
+    console.log(req.body);
+    const newTour = req.body;
+    tours.push(newTour);
+    const jsonText = JSON.stringify(tours);
+    fs.writeFile('./dev-data/data/tours-simple.json',jsonText , (err) => {
+        if(!err) {
+            res
+                .status(201) //HTTP COde 201 = Created
+                .json({
+                    code: 201,
+                    msg: `Add new tour successfully!`,
+                    data: {
+                        tour: newTour
+                    }
+                });
+        } else {
+            res
+                .status(500)    //HTTP COde 500 = Internal Error
+                .json({
+                    code: 500,
+                    msg: `Add new tour fail!`
+                });
+        }
+    });
+}
+
+exports.deleteTour = (req, res) => {
+    console.log(req.params);
+    const id = req.params.id*1;
+    // let index = -1;
+    // for (let i = 0; i < tours.length ; i++){
+    //     const tour = tours[i];
+    //     if (tour.id === id){
+    //         index = i;
+    //         break
+    //     }
+    // }
+    const index = tours.findIndex((tour) => {return tour.id === id});
+    if (index >= 0){
+        tours.splice(index,1);
+        fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(tours) , (err) => {
+            res
+                .status(200)
+                .json({
+                    code: 200,
+                    msg: `Delete id: ${id} successfully!`
+                });
+        })
+    }else{
+        res
+            .status(404) //HTTP CODE 404 -NOT FOUND
+            .json({
+                code: 404,
+                msg: `Not found tour with ${id}!`
+            })
+    }
+}
+
+exports.updateTour = (req, res) => {
+    console.log('Id update', req.params.id);
+    const id = req.params.id * 1;
+    const index = tours.findIndex((tour) => {return tour.id === id});
+    if (index < 0){
+        return res.status(404)
+            .json({
+                code: 404,
+                msg: `Not found tour with ${id}!`
+            })
+    }
+    const updateInfo = req.body;
+    const tourUpdate = tours[index];
+    // console.log('Data update by Id',tourUpdate);
+    // if (updateInfo.name !== null && updateInfo.name !== undefined && updateInfo.name.length>0){
+    if (updateInfo.name){
+        tourUpdate.name = updateInfo.name;
+    }
+    if (typeof updateInfo.duration === 'number' && updateInfo.duration > 0){
+        tourUpdate.duration = updateInfo.duration;
+    }
+    if (typeof updateInfo.maxGroupSize === 'number'  && updateInfo.maxGroupSize > 0){
+        tourUpdate.maxGroupSize = updateInfo.maxGroupSize;
+    }
+
+    fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(tours) , (err) => {
+        res
+            .status(200)
+            .json({
+                code: 200,
+                msg: `Update id: ${id} successfully!`
+            });
+    })
+}
